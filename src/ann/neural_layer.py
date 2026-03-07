a3919fecd4512131619a1d8e4ea884f440ee14b5
Svanik277373 <saisvanik2121@gmail.com>
2026-03-07 23:32:33 +0530

    Update neural_layer.py

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -11,16 +11,17 @@ class NeuralLayer:
         if weight_init == "xavier":
             limit = np.sqrt(6 / (input_dim + output_dim))
             self.W = np.random.uniform(-limit, limit, (input_dim, output_dim))
-
         elif weight_init == "zeros":
             self.W = np.zeros((input_dim, output_dim))
-
         else:
             self.W = np.random.randn(input_dim, output_dim) * 0.01
 
         self.b = np.zeros((1, output_dim))
 
     def forward(self, X):
+
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
 
         self.A_prev = X
         self.Z = X @ self.W + self.b
@@ -30,12 +31,12 @@ class NeuralLayer:
 
         return self.Z
 
-    def backward(self, grad):
+    def backward(self, grad_output):
 
         if self.activation:
-            grad = grad * self.activation.backward(self.Z)
+            grad_output = grad_output * self.activation.backward(self.Z)
 
-        self.grad_W = self.A_prev.T @ grad
-        self.grad_b = np.sum(grad, axis=0, keepdims=True)
+        self.grad_W = self.A_prev.T @ grad_output
+        self.grad_b = np.sum(grad_output, axis=0, keepdims=True)
 
-        return grad @ self.W.T
+        return grad_output @ self.W.T
