b0ff9dc19e529857e8d89127be7c607c552c609e
Svanik277373 <saisvanik2121@gmail.com>
2026-03-07 23:23:27 +0530

    Update neural_layer.py

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -9,31 +9,26 @@ class NeuralLayer:
         self.input_dim = input_dim
         self.output_dim = output_dim
 
-        self.activation_name = activation
         self.activation = Activation(activation) if activation else None
 
         if weight_init == "xavier":
-
             limit = np.sqrt(6 / (input_dim + output_dim))
             self.W = np.random.uniform(-limit, limit, (input_dim, output_dim))
 
         elif weight_init == "zeros":
-
             self.W = np.zeros((input_dim, output_dim))
 
         elif weight_init == "random":
-
             self.W = np.random.randn(input_dim, output_dim) * 0.01
-
-        else:
-            raise ValueError(f"Unknown weight initialization: {weight_init}")
 
         self.b = np.zeros((1, output_dim))
 
     def forward(self, X):
 
-        self.A_prev = X
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
 
+        self.A_prev = X
         self.Z = X @ self.W + self.b
 
         if self.activation:
